7fe1f9034ab78f95fc54d57ef9981df49be44dd4
Salov Konstantin <ferrum9696@mail.ru>
2021-02-20 16:32:39 +0300

    Пользовательский интерфейс

--- app/src/main/java/com/example/hometask_1/MainActivity.kt
+++ app/src/main/java/com/example/hometask_1/MainActivity.kt
@@ -7,20 +7,8 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("netology voice", "start of onCreate function")
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val myText = "Hello, world!"
-        val myNumber = 42
-        val myFloatingNumber = 3.14
-
-        val outputText = myText + " " + myNumber + " " + myFloatingNumber
-
-        val textView = findViewById<TextView>(R.id.text_output)
-        textView.text = outputText
-
-        Log.d("netology voice", "end of onCreate function")
+        setSupportActionBar(findViewById(R.id.topAppBar))
     }
 }
